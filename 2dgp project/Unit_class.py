d9ae67353c2932aa7135f0585af3ffe550908342
kakik <39730096+kakik@users.noreply.github.com>
2018-11-08 14:27:35 +0900

    draw collision_check box

--- 2dgp project/Unit_class.py
+++ 2dgp project/Unit_class.py
@@ -27,6 +27,7 @@ class Player():
 
     def update(self):
         self.unit.move()
+        self.unit.collision_check()
         # 이동 종료
         if (self.unit.curr_t) > self.unit.to_t:
             self.unit.to_x = self.unit.x
@@ -92,6 +93,7 @@ class Observer():
             Observer.image[(int)(28- self.frame.current_frame)].composite_draw(3.141595653589793238,'v', self.x - game_world.screen_coord_x, self.y - game_world.screen_coord_y, self.width, self.height)
         else:
             Observer.image[(int)(self.frame.current_frame)].draw(self.x - game_world.screen_coord_x, self.y - game_world.screen_coord_y)
+        draw_rectangle(self.x-self.width/2,self.y-self.height/2,self.x+self.width/2,self.y+self.height/2)
 
     def set_random_move_point(self):
         x_distance = random.randint(0, 1000) - 500
@@ -193,6 +195,8 @@ class Wraith():
                                                                               self.y - game_world.screen_coord_y, self.width, self.height)
         else:
             Wraith.image[(int)(self.frame.current_frame)].draw(self.x - game_world.screen_coord_x, self.y - game_world.screen_coord_y)
+        draw_rectangle(self.x - self.width / 2, self.y - self.height / 2, self.x + self.width / 2,
+                       self.y + self.height / 2)
 
     def set_random_move_point(self):
         x_distance = random.randint(0, 1000) - 500
@@ -301,6 +305,8 @@ class Scourge():
         else:
             Scourge.image.clip_draw((self.width+3) * (int)(self.frame.current_frame) + 2, 369 - ((self.height+3)*(int)(self.IDLE_frame.current_frame+1)-1), self.width, self.height - 2, self.x - game_world.screen_coord_x,
                                     self.y - game_world.screen_coord_y)
+        draw_rectangle(self.x - self.width / 2, self.y - self.height / 2, self.x + self.width / 2,
+                       self.y + self.height / 2)
 
     def set_random_move_point(self):
         x_distance = random.randint(0, 1000) - 500
