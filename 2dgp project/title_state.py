c440b71311eedd8eda4fc2db117b4be352837b7a
kakik <39730096+kakik@users.noreply.github.com>
2018-11-27 01:31:36 +0900

    change some variables' name

--- 2dgp project/title_state.py
+++ 2dgp project/title_state.py
@@ -24,7 +24,8 @@ def enter():
     global Title_start, Title_start_mouse_on
     global Title_exit, Title_exit_mouse_on
 
-    Title_BG_img = load_image('resources\\Title\\Background\\Title_BG.png')
+    Title_BG_img = MenuBGIMG()
+    game_world.add_object(Title_BG_img, 0)
     Title_start_text_img = load_image('resources\\Title\\Start\\start.png')
     Title_exit_text_img = load_image('resources\\Title\\Exit\\exit.png')
 
@@ -34,10 +35,10 @@ def enter():
     game_world.my = game_world.screen_y/2
 
     # 객체 생성!
-    Title_start = IMG_class.MenuStartIMG()
-    Title_start_mouse_on =IMG_class.MenuStartMouseOnIMG()
-    Title_exit = IMG_class.MenuExitIMG()
-    Title_exit_mouse_on = IMG_class.MenuExitMouseOnIMG()
+    Title_start = MenuStartIMG()
+    Title_start_mouse_on =MenuStartMouseOnIMG()
+    Title_exit = MenuExitIMG()
+    Title_exit_mouse_on = MenuExitMouseOnIMG()
 
     game_world.add_object(Title_start,0)
     game_world.add_object(Title_start_mouse_on,0)
@@ -53,11 +54,10 @@ def exit():
     global Title_start_text_img, Title_exit_text_img
     global Title_start, Title_start_mouse_on
     global Title_exit, Title_exit_mouse_on
-    del Title_BG_img
     del Title_start_text_img
     del Title_exit_text_img
 
-    #game_world.clear()
+    game_world.remove_object(Title_BG_img)
     game_world.remove_object(Title_start)
     game_world.remove_object(Title_start_mouse_on)
     game_world.remove_object(Title_exit)
@@ -105,8 +105,6 @@ def draw():
     global Title_exit, Title_exit_mouse_on
 
     clear_canvas()
-    if  Title_BG_img!=None:
-        Title_BG_img.clip_draw(0,0,game_world.screen_x,game_world.screen_y,game_world.screen_x/2,game_world.screen_y/2)
 
     for game_object in game_world.all_objects():
         game_object.draw()
@@ -126,6 +124,167 @@ def pause():
 
 def resume():
     pass
+
+
+
+class MenuBGIMG():
+    image = None
+    width = 800
+    height = 600
+
+    def __init__(self):
+        if self.image == None:
+            self.image = load_image('resources\\Title\\Background\\Title_BG.png')
+
+    def handle_events(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        if self.image != None:
+            self.image.clip_draw(0, 0, game_world.screen_x, game_world.screen_y, game_world.screen_x / 2, game_world.screen_y / 2)
+
+class MenuStartIMG():
+    image = None
+    width = 320
+    height = 116
+    max_frame = 35
+    ACTION_PER_TIME = 0.5
+
+    def __init__(self):
+        self.x = 240
+        self.y = game_world.screen_y - 162
+
+        self.frame = IMG_class.Frame(self.max_frame,self.ACTION_PER_TIME)
+        self.mouse_on = False
+
+        if self.image == None:
+            self.image = [load_image('resources\\Title\\Start\\single%d%d.png'%(i//10, i%10)) for i in range(0, self.max_frame)]
+
+
+    def handle_events(self):
+        pass
+
+    def update(self):
+        self.frame.update()
+        if (self.x - self.width / 2 <= game_world.mx <= self.x + self.width / 2) and (
+                self.y - self.height / 2 <= game_world.my <= self.y + self.height / 2):
+            if self.mouse_on == False:
+                self.mouse_on = True
+        else:
+            if self.mouse_on == True:
+                self.mouse_on = False
+
+    def draw(self):
+            self.image[(int)(self.frame.current_frame)].draw(self.x, self.y)
+
+
+
+class MenuExitIMG():
+    image = None
+    width = 184
+    height = 128
+    max_frame = 50
+    ACTION_PER_TIME = 0.5
+
+    def __init__(self):
+        self.x = game_world.screen_x - 208
+        self.y = 122
+
+        self.frame = IMG_class.Frame(self.max_frame,self.ACTION_PER_TIME)
+        self.mouse_on = False
+
+        if self.image == None:
+            self.image = [load_image('resources\\Title\\Exit\\exit%d%d.png' % (i // 10, i % 10))  for i in range(0, self.max_frame)]
+
+    def handle_events(self):
+        pass
+
+    def update(self):
+        self.frame.update()
+        if (self.x - self.width / 2 <= game_world.mx <= self.x + self.width / 2) and (
+                self.y - self.height / 2 <= game_world.my <= self.y + self.height / 2):
+            if self.mouse_on == False:
+                self.mouse_on = True
+        else:
+            if self.mouse_on == True:
+                self.mouse_on = False
+
+    def draw(self):
+            self.image[(int)(self.frame.current_frame)].draw(self.x, self.y)
+
+
+class MenuStartMouseOnIMG():
+    image = None
+    width = 320
+    height = 116
+    max_frame = 60
+    ACTION_PER_TIME = 0.5
+
+    def __init__(self):
+        self.x = 240
+        self.y = game_world.screen_y - 162
+
+        self.frame = IMG_class.Frame(self.max_frame,self.ACTION_PER_TIME)
+        self.mouse_on = False
+
+        if self.image == None:
+            self.image =[load_image('resources\\Title\\Start\\English\\singleon%d%d.png' % (i // 10, i % 10)) for i in range(0, self.max_frame)]
+
+    def handle_events(self):
+        pass
+
+    def update(self):
+        self.frame.update()
+        if (self.x - self.width / 2 <= game_world.mx <= self.x + self.width / 2) and (
+                self.y - self.height / 2 <= game_world.my <= self.y + self.height / 2):
+            if self.mouse_on == False:
+                self.mouse_on = True
+                self.frame.current_frame = 0
+        else:
+            if self.mouse_on == True:
+                self.mouse_on = False
+
+    def draw(self):
+        if self.mouse_on == True:
+            self.image[(int)(self.frame.current_frame)].draw(self.x, self.y)
+
+class MenuExitMouseOnIMG():
+    image = None
+    width = 184
+    height = 128
+    max_frame = 30
+    ACTION_PER_TIME = 0.5
+
+    def __init__(self):
+        self.x = game_world.screen_x - 208
+        self.y = 122
+
+        self.frame = IMG_class.Frame(self.max_frame,self.ACTION_PER_TIME)
+        self.mouse_on = False
+
+        if self.image == None:
+            self.image = [load_image('resources\\Title\\Exit\\English\\exiton%d%d.png'%(i//10, i%10)) for i in range(0, self.max_frame)]
+
+    def handle_events(self):
+        pass
+
+    def update(self):
+        self.frame.update()
+        if (self.x - self.width / 2 <= game_world.mx <= self.x + self.width / 2) and (
+                self.y - self.height / 2 <= game_world.my <= self.y + self.height / 2):
+            if self.mouse_on == False:
+                self.mouse_on = True
+                self.frame.current_frame = 0
+        else:
+            if self.mouse_on == True:
+                self.mouse_on = False
+
+    def draw(self):
+        if self.mouse_on == True:
+            self.image[(int)(self.frame.current_frame)].draw(self.x, self.y)
 
 
 
