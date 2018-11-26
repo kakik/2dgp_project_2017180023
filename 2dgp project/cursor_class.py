ce913c1912b1cec787706d1b131a70d77963e16a
kakik <39730096+kakik@users.noreply.github.com>
2018-11-27 02:09:11 +0900

    draw cursor_image and cursor_clicked_image separately

--- 2dgp project/cursor_class.py
+++ 2dgp project/cursor_class.py
@@ -8,7 +8,7 @@ class Cursor:
     width = 41
     height = 41
     max_frame = 5
-    ACTION_PER_TIME = 1
+    ACTION_PER_TIME = 0.8
 
 
     def __init__(self, x, y):
@@ -17,10 +17,14 @@ class Cursor:
         self.frame = IMG_class.Frame(5,self.ACTION_PER_TIME)
         if self.image == None:
             Cursor.image = load_image('resources\\Cursor\\cursor.png')
+        self.cursor_clicked_x = 0
+        self.cursor_clicked_y = 0
         self.cursor_clicked_animation = 0
         self.cursor_clicked_animation_size = 1.0
 
     def get_events(self):
+        self.cursor_clicked_x = self.x
+        self.cursor_clicked_y = self.y
         self.cursor_clicked_animation = 1
         self.cursor_clicked_animation_size = 1.0
 
@@ -44,6 +48,6 @@ class Cursor:
 
     def draw(self):
         if self.cursor_clicked_animation != 0:
-            self.image.clip_draw(46, 232, 41, 41, self.x - game_world.screen_coord_x, self.y - game_world.screen_coord_y, self.width * self.cursor_clicked_animation_size, self.height * self.cursor_clicked_animation_size)
+            self.image.clip_draw(46, 232, 41, 41, self.cursor_clicked_x - game_world.screen_coord_x, self.cursor_clicked_y - game_world.screen_coord_y, self.width * self.cursor_clicked_animation_size, self.height * self.cursor_clicked_animation_size)
         self.image.clip_draw((self.width+3) * (int)(self.frame.current_frame) + 2 , 279, self.width, self.height, self.x - game_world.screen_coord_x, self.y - game_world.screen_coord_y)
 
