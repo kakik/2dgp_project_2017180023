ce6efbd3425fb5ef7868c6b415426f13f7ee55ee
kakik <39730096+kakik@users.noreply.github.com>
2018-12-01 23:30:59 +0900

    complete result_stage

--- 2dgp project/result_state.py
+++ 2dgp project/result_state.py
@@ -8,25 +8,17 @@ import title_state
 name = "ResultState"
 
 Result_BG_IMG = None
-Result_exit_text_img = None
-Result_exit_img = None
-Result_exit_mouse_on_img = None
 Result_score_popup = None
 
 def enter():
     global Result_BG_IMG
-    global Result_exit_text_img,Result_exit_img,Result_exit_mouse_on_img
     global Result_score_popup
 
     Result_BG_IMG = ResultBGIMG()
-    Result_exit_img = ResultExitIMG()
-    Result_exit_mouse_on_img = ResultExitMouseOnIMG()
-    Result_exit_text_img = ResultExitText()
+
     Result_score_popup= ResultScorePopup()
     game_world.add_object(Result_BG_IMG, 0)
-    game_world.add_object(Result_exit_img, 0)
-    game_world.add_object(Result_exit_mouse_on_img, 0)
-    game_world.add_object(Result_exit_text_img, 0)
+
     game_world.add_object(Result_score_popup, 1)
 
 def exit():
@@ -38,7 +30,7 @@ def update():
         game_object.update()
 
 def handle_events():
-    global Result_exit_img
+    global Result_score_popup
 
     events = get_events()
     for event in events:
@@ -48,8 +40,8 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 game_framework.quit()
         elif event.type == SDL_MOUSEBUTTONDOWN:
-             if Result_exit_img.mouse_on is True:
-                 game_framework.change_state(title_state)
+            if Result_score_popup.is_mouse_on is True:
+                game_framework.change_state(title_state)
         elif event.type == SDL_MOUSEMOTION:
                 # 마우스 좌표 업데이트
                 game_world.update_mouse_point(event.x, event.y)
@@ -110,13 +102,14 @@ class ResultScorePopup():
             self.draw_height = self.height / game_world.screen_y * 600
         if self.move_down_speed_per_sec is None:
             self.move_down_speed_per_sec = game_world.screen_y*1.5
-
         if self.image == None:
             self.image = load_image('resources\\UI\\popopup.png')
-            self.x = game_world.screen_x/2
-            self.y = game_world.screen_y + self.draw_height/2
-            self.font = load_font('font\\starcraft.TTF', 20)
-            self.clear_stage = main_state.stage_level - 1
+
+        self.x = game_world.screen_x/2
+        self.y = game_world.screen_y + self.draw_height/2
+        self.font = load_font('font\\starcraft.TTF', 20)
+        self.clear_stage = main_state.stage_level - 1
+        self.is_mouse_on = False
 
 
 
@@ -129,100 +122,21 @@ class ResultScorePopup():
             if self.y < game_world.screen_y /2:
                 self.y = game_world.screen_y / 2
 
+        if (self.x - self.width / 2 <= game_world.mx <= self.x + self.width / 2) and (
+                self.y - self.height / 2 <= game_world.my <= self.y + self.height / 2):
+            if self.is_mouse_on is False:
+                self.is_mouse_on = True
+        else:
+            if self.is_mouse_on is True:
+                self.is_mouse_on = False
+
     def draw(self):
         if self.image != None:
             self.image.clip_draw(0, 0, self.width, self.height, self.x,
                                  int(self.y),self.draw_width, self.draw_height)
 
-        self.font.draw( self.x-50, self.y, 'stage: %d' % (main_state.stage_level), (0, 255, 0))
+        self.font.draw( self.x-100, self.y, 'Clear Stage: %d' % (self.clear_stage), (0, 255, 0))
 
-
-
-class  ResultExitIMG():
-    image = None
-    width = 184
-    height = 128
-    max_frame = 50
-    ACTION_PER_TIME = 0.5
-
-    def __init__(self):
-        self.x = game_world.screen_x - 158
-        self.y = 122
-        self.frame = IMG_class.Frame(self.max_frame,self.ACTION_PER_TIME)
-        self.mouse_on = False
-
-        if self.image == None:
-            self.image = [load_image('resources\\Title\\Exit\\exit%d%d.png' % (i // 10, i % 10))  for i in range(0, self.max_frame)]
-
-    def handle_events(self):
-        pass
-
-    def update(self):
-        self.frame.update()
-        if (self.x - self.width / 2 <= game_world.mx <= self.x + self.width / 2) and (
-                self.y - self.height / 2 <= game_world.my <= self.y + self.height / 2):
-            if self.mouse_on == False:
-                self.mouse_on = True
-        else:
-            if self.mouse_on == True:
-                self.mouse_on = False
-
-    def draw(self):
-            self.image[(int)(self.frame.current_frame)].draw(self.x, self.y)
-
-class  ResultExitMouseOnIMG():
-    image = None
-    width = 184
-    height = 128
-    max_frame = 30
-    ACTION_PER_TIME = 0.5
-
-    def __init__(self):
-        self.x = game_world.screen_x - 158
-        self.y = 122
-
-        self.frame = IMG_class.Frame(self.max_frame,self.ACTION_PER_TIME)
-        self.mouse_on = False
-
-        if self.image == None:
-            self.image = [load_image('resources\\Title\\Exit\\English\\exiton%d%d.png'%(i//10, i%10)) for i in range(0, self.max_frame)]
-
-    def handle_events(self):
-        pass
-
-    def update(self):
-        self.frame.update()
-        if (self.x - self.width / 2 <= game_world.mx <= self.x + self.width / 2) and (
-                self.y - self.height / 2 <= game_world.my <= self.y + self.height / 2):
-            if self.mouse_on == False:
-                self.mouse_on = True
-                self.frame.current_frame = 0
-        else:
-            if self.mouse_on == True:
-                self.mouse_on = False
-
-    def draw(self):
-        if self.mouse_on == True:
-            self.image[(int)(self.frame.current_frame)].draw(self.x, self.y)
-
-class  ResultExitText():
-    image = None
-    width = 200
-    height = 100
-
-    def __init__(self):
-        if self.image == None:
-            self.image = load_image('resources\\Title\\Exit\\exit.png')
-
-    def handle_events(self):
-        pass
-
-    def update(self):
-        pass
-
-    def draw(self):
-        if self.image != None:
-            self.image.draw(game_world.screen_x - 200, 180)
 
 
 ############################################################################################################################################################
